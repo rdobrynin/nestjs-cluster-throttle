eb00dcb6ab6917587fb4acf6b7b7052358b73591
Roman Dobrynin <roman.dobrynin@gmail.com>
2025-12-15 18:37:35 +0300

    add: CODEOWNERS

--- dist/decorators/rate-limit.decorator.d.ts
+++
@@ -1,4 +0,0 @@
-import { RateLimitOptions } from '../interfaces/rate-limit-options.interface';
-export declare const RATE_LIMIT_METADATA = "RATE_LIMIT_METADATA";
-export declare const RateLimit: (options: RateLimitOptions) => import("@nestjs/common").CustomDecorator<string>;
-//# sourceMappingURL=rate-limit.decorator.d.ts.map
--- dist/decorators/rate-limit.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RateLimit = exports.RATE_LIMIT_METADATA = void 0;
-const common_1 = require("@nestjs/common");
-exports.RATE_LIMIT_METADATA = 'RATE_LIMIT_METADATA';
-const RateLimit = (options) => (0, common_1.SetMetadata)(exports.RATE_LIMIT_METADATA, options);
-exports.RateLimit = RateLimit;
-//# sourceMappingURL=rate-limit.decorator.js.map
